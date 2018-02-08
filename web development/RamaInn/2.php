b90d3e98ee81ca94c4cd77d1bc0ca15c54e8ff85
Nisha Patel <nisdpate@ut.utm.edu>
2018-02-08 13:10:02 -0600

    changes

--- web development/RamaInn/2.php
+++ web development/RamaInn/2.php
@@ -5,7 +5,7 @@
 		$message = $_POST['message'];
 		$human = intval($_POST['human']);
 		$from = 'Demo Contact Form'; 
-		$to = 'nis.patel23@gmail.com'; 
+		$to = 'email@email.com'; 
 		$subject = 'Message from Contact Demo ';
 		
 		$body = "From: $name\n E-Mail: $email\n Message:\n $message";
